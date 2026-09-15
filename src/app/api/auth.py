de18a04bf7b3b799e3a4ffaeb5ac8252230f0541
Giorgi Serobyan <giorgi.serobyan.1@gmail.com>
2026-09-15 15:42:54 +0400

    finish jwt token, add login endpoint

--- src/app/api/auth.py
+++ src/app/api/auth.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
-
+from app.api.dependencies import LoginForm, AuthService
+from app.schemas.api.token import Token
 router = APIRouter()
 
 
-router.post('login')
+@router.post('/login', response_model=Token)
+async def login_for_access_token(auth_serv: AuthService, user_form: LoginForm):
+
+	username, password = user_form.username, user_form.password
+
+	jwt_token = await auth_serv.login_for_access_token(username, password)
+
+	return Token(access_token=jwt_token, token_type="bearer")
